a396b95fa07d82f58e2d2ea2e087d9732a37467e
pnilsssson <p.nilsson95@hotmail.com>
2024-04-03 14:58:35 +0200

    Initial files

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth"
+import EmailProvider from "next-auth/providers/email"
+
+const handler = NextAuth({
+    providers: [
+        EmailProvider({
+          server: process.env.EMAIL_SERVER,
+          from: process.env.EMAIL_FROM
+        }),
+      ],
+})
+
+export { handler as GET, handler as POST }

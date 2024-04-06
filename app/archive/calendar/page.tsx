718fa0c968307fbacabfdd4a4c6127121266e3f2
pnilsssson <p.nilsson95@hotmail.com>
2024-04-06 21:45:43 +0200

    Test logging.

--- app/archive/calendar/page.tsx
+++ app/archive/calendar/page.tsx
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 const Calendar = dynamic(() => import("./calendar"), { ssr: false });
 
 export default function Page() {
+  console.log(process.env.NEXT_PUBLIC_URL);
   return (
     <div>
       <h1 className="text-4xl font-bold">Calendar</h1>
